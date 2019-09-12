c03c444b5cae6f042878241e0e7100b9f809db24
andreasvc <a.w.van.cranenburgh@rug.nl>
2019-09-12 17:39:59 +0200

    Fix SemEval test data as well
    - correct typo in SemEval dev parse: Löbecker -> Lübecker
    - remove redundant files with tokenized sentences of SemEval dev data

--- fixsemeval2010.sh
+++ fixsemeval2010.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
 # Download SemEval 2010 shared task data
 # http://www.lsi.upc.edu/~esapena/downloads/index.php?id=1
-sed 's/‚Äö√Ñ¬∞/à/g; s/¬¨‚àë/á/g; s/‚àö√©/ë/g; s/‚àö√†/é/g; s/‚àö√£/è/g; s/‚àö√ß/ê/g; s/‚àö√Æ/ï/g; s/¬¨‚àè/ü/g; s/√Ä√ú/ö/g; s/‚àö√µ/ó/g; s/‚àö‚àë/Ö/g; s/‚àö√Ö/ç/g;' < nl.devel.txt > nl.devel.txt.fixed
+# Fixes encoding errors, as well as syntax errors in the coreference column.
+for FILE in task01.posttask.v1.0/corpora/*/nl.*.txt
+do
+	sed 's/‚Äö√Ñ¬∞/à/g;
+		s/¬¨‚àë/á/g;
+		s/‚àö√©/ë/g;
+		s/‚àö√†/é/g;
+		s/‚àö√£/è/g;
+		s/‚àö√ß/ê/g;
+		s/‚àö√Æ/ï/g;
+		s/¬¨‚àè/ü/g;
+		s/√Ä√ú/ö/g;
+		s/‚àö√µ/ó/g;
+		s/‚àö‚àë/Ö/g;
+		s/‚àö√Ö/ç/g;
+
+		s/-\([()|0-9]\+\)$/\1/g;
+		s/)(\([()|0-9]\+\)$/)|(\1/g;
+		s/LET()$\|+$/_/;' < $FILE > $FILE.fixed
+done
